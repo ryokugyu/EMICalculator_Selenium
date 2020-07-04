2fafdddd45ecd222e5d37c25e4985601672ae4b0
Himanshu Nailwal <himanshunailwal.rhce@gmail.com>
2020-07-04 18:54:10 +0530

    invalid tests added

--- src/test/java/com/InvalidSmokeSuiteTests/InvalidPageTitle.java
+++ src/test/java/com/InvalidSmokeSuiteTests/InvalidPageTitle.java
@@ -1,4 +1,4 @@
-package com.SmokeTestSuiteCLass;
+package com.InvalidSmokeSuiteTests;
 
 import java.util.Hashtable;
 import java.util.Properties;
@@ -12,19 +12,20 @@ import com.pageClasses.LandingPage;
 import com.utilities.ReadPropertiesFile;
 import com.utilities.TestDataProvider;
 
-public class VerifyPageTitle extends baseTestClass {
+public class InvalidPageTitle extends baseTestClass {
 
 	Properties prop = ReadPropertiesFile.readConfiguration();
 
 	/******************* Page Title Test *******************/
-	@Test(dataProvider = "EMICalculatorTestSmokeSuiteDriverTitleData", description = "checks the webpage title")
+	@Test(dataProvider = "EMICalculatorTestInvalidSmokeSuiteDriverTitleData", description = "checks the webpage title")
 	public void verifyPageTitle(Hashtable<String, String> testData) {
 		LandingPage landingPage;
 
 		System.out.println("******Executing verifyPageTitle Test******");
 
 		// creating test for logger to record
-		logger = report.createTest("EMICalculator Page title Test");
+		logger = report.createTest("EMICalculator Invalid Page title Test");
+
 		// initializing the browser
 		invokeBrowser(prop.getProperty("browserName"));
 		landingPage = openApplication(prop.getProperty("WebPageURL"));
@@ -32,9 +33,11 @@ public class VerifyPageTitle extends baseTestClass {
 
 		logger.log(Status.PASS, "EMICalculator webpage title test passed successfuly");
 	}
-	
+
 	@DataProvider
-	public Object[][] EMICalculatorTestSmokeSuiteDriverTitleData() {
-		return TestDataProvider.getTestData("EMICalculatorTestData.xlsx", "SmokeSuiteTestData", "driverTitleTestData");
+	public Object[][] EMICalculatorTestInvalidSmokeSuiteDriverTitleData() {
+		return TestDataProvider.getTestData("EMICalculatorTestData.xlsx", "InvalidSmokeSuiteTestData",
+				"driverTitleTestData");
 	}
+
 }
